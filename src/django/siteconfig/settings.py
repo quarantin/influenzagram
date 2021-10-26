6f501086d1fcb9ad9b8ea001665b351edc309d20
Corentin Delorme <codelorme@gmail.com>
2021-10-27 01:36:48 +0200

    Update Django settings.py: Add influenzagram to INSTALLED_APPS

--- src/django/siteconfig/settings.py
+++ src/django/siteconfig/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'influenzagram',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
