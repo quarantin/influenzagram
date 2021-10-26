c0a1227844b78704c3f81017b84369b0d368c60e
Corentin Delorme <codelorme@gmail.com>
2021-10-27 00:19:20 +0200

    Add Django files

---
+++ influenzagram/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for influenzagram project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'influenzagram.settings')
+
+application = get_asgi_application()
