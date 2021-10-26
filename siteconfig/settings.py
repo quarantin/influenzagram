e4663bda8e893ec2dfb0a5462f3222b45206459a
Corentin Delorme <codelorme@gmail.com>
2021-10-27 00:29:03 +0200

    Rename influenzagram folder to siteconfig

--- siteconfig/settings.py
+++ siteconfig/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'influenzagram.urls'
+ROOT_URLCONF = 'siteconfig.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'influenzagram.wsgi.application'
+WSGI_APPLICATION = 'siteconfig.wsgi.application'
 
 
 # Database
