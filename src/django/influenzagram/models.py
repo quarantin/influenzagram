2c74097b7118de50310e946a959e2c28dab582f8
Corentin Delorme <codelorme@gmail.com>
2021-10-27 03:24:11 +0200

    Update Django models: Change field Target.birth_data from type DateTimeField to DateField

--- src/django/influenzagram/models.py
+++ src/django/influenzagram/models.py
@@ -7,7 +7,7 @@ class Target(models.Model):
 	first_name = models.CharField(max_length=32)
 	last_name = models.CharField(max_length=32)
 	country = models.CharField(max_length=32)
-	birth_date = models.DateTimeField()
+	birth_date = models.DateField()
 
 	def __str__(self):
 		return self.first_name + ' ' + self.last_name + ' (' + self.country + ')'
