eba88953f545ef4ca7485c3bc5996fc736c235ff
Corentin Delorme <codelorme@gmail.com>
2021-10-27 03:06:52 +0200

    Update Django admin: Register models in admin site

--- src/django/influenzagram/admin.py
+++ src/django/influenzagram/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+class TargetAdmin(admin.ModelAdmin):
+    pass
+
+class DataProviderAdmin(admin.ModelAdmin):
+	pass
+
+class DataProviderSecretAdmin(admin.ModelAdmin):
+	pass
+
+class DataSourceAdmin(admin.ModelAdmin):
+	pass
+
+class OnlinePresenceAdmin(admin.ModelAdmin):
+	pass
+
+class PictureAdmin(admin.ModelAdmin):
+	pass
+
+class ProfilePictureAdmin(admin.ModelAdmin):
+	pass
+
+class TagAdmin(admin.ModelAdmin):
+	pass
+
+class TagAssocAdmin(admin.ModelAdmin):
+	pass
+ 
+class VerificationAdmin(admin.ModelAdmin):
+	pass
+
+class VideoAdmin(admin.ModelAdmin):
+	pass
+
+admin.site.register(Target, TargetAdmin)
+admin.site.register(DataProvider, DataProviderAdmin)
+admin.site.register(DataProviderSecret, DataProviderSecretAdmin)
+admin.site.register(DataSource, DataSourceAdmin)
+admin.site.register(OnlinePresence, OnlinePresenceAdmin)
+admin.site.register(Picture, PictureAdmin)
+admin.site.register(ProfilePicture, ProfilePictureAdmin)
+admin.site.register(Tag, TagAdmin)
+admin.site.register(TagAssoc, TagAssocAdmin)
