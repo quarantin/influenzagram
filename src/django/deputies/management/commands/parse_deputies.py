af5c9ed81337842e76897c4d9c8f220771aa6403
Corentin Delorme <codelorme@gmail.com>
2021-10-28 21:00:12 +0200

    Add new Django app: deputies

---
+++ src/django/deputies/management/commands/parse_deputies.py
@@ -0,0 +1,258 @@
+import os
+import json
+import requests
+import zipfile
+
+from django.core.management.base import BaseCommand
+from deputies.models import *
+
+
+class Command(BaseCommand):
+
+	def debug(self, string):
+		self.fd.write(string + '\n')
+
+	def download_data(self):
+		if not os.path.exists(self.filepath):
+			print('Downloading ' + self.url)
+			response = requests.get(self.url)
+			fd = open(self.filepath, 'wb')
+			fd.write(response.content)
+			fd.close()
+
+	def extract_data(self):
+		if not os.path.exists(self.jsonfolder):
+			print('Extracting ' + self.filepath)
+			with zipfile.ZipFile(self.filepath) as fd:
+				fd.extractall(self.folder)
+
+	def parse_organ(self, jsonfile, parents):
+
+		print('Parsing organ ' + os.path.basename(jsonfile))
+		with open(jsonfile, 'r') as fd:
+			data = json.loads(fd.read())
+
+		organ = data['organe']
+
+		info = {
+			'uid':            organ['uid'],
+			'type':           organ['@xsi:type'],
+			'type_code':      organ['codeType'],
+			'label':          organ['libelle'],
+			'label_edition':  organ['libelleEdition'],
+			'label_short':    organ['libelleAbrege'],
+			'label_abbrev':   organ['libelleAbrev'],
+			'date_start':     organ['viMoDe']['dateDebut'],
+			'date_agreement': organ['viMoDe']['dateAgrement'],
+			'date_end':       organ['viMoDe']['dateFin'],
+			'chamber':        organ.get('chambre'),
+			'regime':         organ.get('regime'),
+			'legislature':    organ.get('legislature'),
+			'secretary_1':    organ.get('secretariat', {}).get('secretaire01'),
+			'secretary_2':    organ.get('secretariat', {}).get('secretaire02'),
+		}
+
+		organ, created = Organ.objects.get_or_create(**info)
+
+		if organ.parent_organ:
+			parents[organ.pk] = organ['parent_organ']
+
+	def parse_parent_organs(self, parents):
+		for organ_pk, parent_uid in parents.items():
+			organ = Organ.objects.get(pk=organ_pk)
+			organ.parent_organ = Organ.objects.get(uid=parent_uid)
+			organ.save()
+
+	def parse_address(self, deputy, addr):
+
+		info = {
+			'deputy':      deputy,
+			'type':        addr['type'],
+			'type_label':  addr['typeLibelle'],
+			'weight':      addr['poids'],
+			'address_uid': addr['adresseDeRattachement'],
+		}
+
+		if addr['@xsi:type'] == 'AdresseMail_Type':
+			info['email_address']   = addr['valElec']
+
+		elif addr['@xsi:type'] == 'AdressePostale_Type':
+			info['entitle']         = addr['intitule']
+			info['street_number']   = addr['numeroRue']
+			info['street_name']     = addr['nomRue']
+			info['street_addition'] = addr['complementAdresse']
+			info['postal_code']     = addr['codePostal']
+			info['city']            = addr['ville']
+
+		elif addr['@xsi:type'] == 'AdresseSiteWeb_Type':
+			info['web_site']        = addr['valElec']
+
+		elif addr['@xsi:type'] == 'AdresseTelephonique_Type':
+			info['phone_number']    = addr['valElec']
+
+		address, created = DeputyAddress.objects.get_or_create(**info)
+
+	def parse_mandate(self, deputy, mandate):
+
+		info = {
+			'deputy':                     deputy,
+			'uid':                        mandate['uid'],
+			'mandate_type':               mandate['@xsi:type'],
+			'legislature':                mandate['legislature'],
+			'date_start':                 mandate['dateDebut'],
+			'date_published':             mandate['datePublication'],
+			'date_end':                   mandate['dateFin'],
+			'precedence':                 mandate['preseance'],
+			'main_nomination':            mandate['nominPrincipale'],
+			'quality_code':               mandate['infosQualite']['codeQualite'],
+			'quality_label':              mandate['infosQualite']['libQualite'],
+			'quality_label_sex':          mandate['infosQualite']['libQualiteSex'],
+			'chamber':                    mandate.get('chambre'),
+			'election_region':            mandate.get('election', {}).get('lieu', {}).get('region'),
+			'election_region_type':       mandate.get('election', {}).get('lieu', {}).get('regionType'),
+			'election_department':        mandate.get('election', {}).get('lieu', {}).get('departement'),
+			'election_department_number': mandate.get('election', {}).get('lieu', {}).get('numDepartement'),
+			'election_circonscription':   mandate.get('election', {}).get('lieu', {}).get('numCirco'),
+			'mandate_cause':              mandate.get('election', {}).get('causeMandat'),
+			'circonscription_uid':        mandate.get('election', {}).get('refCirconscription'),
+			'mandate_date_start':         mandate.get('mandature', {}).get('datePriseFonction'),
+			'mandate_end_cause':          mandate.get('mandature', {}).get('causeFin'),
+			'mandate_first_election':     mandate.get('mandature', {}).get('premiereElection'),
+			'mandate_hemicycle_seat':     mandate.get('mandature', {}).get('placeHemicycle'),
+			'mandate_replace_uid':        mandate.get('mandature', {}).get('mandatRemplaceRef'),
+		}
+
+		if 'suppleants' in mandate and mandate['suppleants']:
+			info['suppleant_date_start'] = mandate['suppleants']['suppleant']['dateDebut']
+			info['suppleant_date_end']   = mandate['suppleants']['suppleant']['dateFin']
+			info['suppleant_uid']        = mandate['suppleants']['suppleant']['suppleantRef']
+
+		try:
+			mandate_obj, created = Mandate.objects.get_or_create(**info)
+		except Exception as err:
+			self.debug(str(err))
+			self.debug(json.dumps(info, indent=4))
+			return # TODO remove this line
+
+		self.parse_mandate_organs(mandate_obj, mandate['organes']['organeRef'])
+
+		if 'collaborateurs' in mandate and mandate['collaborateurs'] and type(mandate['collaborateurs']) == dict:
+			if type(mandate['collaborateurs']['collaborateur']) == list:
+				for collaborater in mandate['collaborateurs']['collaborateur']:
+					self.parse_mandate_collaborater(deputy, mandate_obj, collaborater)
+			else:
+				self.parse_mandate_collaborater(deputy, mandate_obj, mandate['collaborateurs']['collaborateur'])
+
+	def parse_mandate_organ(self, mandate, organ_uid):
+
+		try:
+			organ = Organ.objects.get(uid=organ_uid)
+
+		except Organ.DoesNotExist:
+			self.debug('WARN: Missing organ: ' + organ_uid)
+			organ = None
+
+		MandateOrgan.objects.get_or_create(mandate=mandate, organ=organ, organ_uid=organ_uid)
+
+	def parse_mandate_organs(self, mandate, organs):
+		if type(organs) == list:
+			for organ_uid in organs:
+				self.parse_mandate_organ(mandate, organ_uid)
+		else:
+			self.parse_mandate_organ(mandate, organs)
+
+	def parse_mandate_collaborater(self, deputy, mandate, collaborater):
+
+		info = {
+			'deputy':     deputy,
+			'mandate':    mandate,
+			'civility':   collaborater['qualite'],
+			'first_name': collaborater['prenom'],
+			'last_name':  collaborater['nom'],
+			'date_start': collaborater['dateDebut'],
+			'date_end':   collaborater['dateFin'],
+		}
+
+		MandateCollaborater.objects.get_or_create(**info)
+
+	def parse_actor(self, jsonfile):
+
+		print('Parsing actor ' + os.path.basename(jsonfile))
+		with open(jsonfile, 'r') as fd:
+			data = json.loads(fd.read())
+
+		actor = data['acteur']
+
+		info = {
+			'uid':              actor['uid']['#text'],
+			'civility':         actor['etatCivil']['ident']['civ'],
+			'first_name':       actor['etatCivil']['ident']['prenom'],
+			'last_name':        actor['etatCivil']['ident']['nom'],
+			'alpha':            actor['etatCivil']['ident']['alpha'],
+			'trigram':          actor['etatCivil']['ident']['trigramme'],
+			'birth_date':       actor['etatCivil']['infoNaissance']['dateNais'],
+			'birth_city':       actor['etatCivil']['infoNaissance']['villeNais'],
+			'birth_department': actor['etatCivil']['infoNaissance']['depNais'],
+			'birth_country':    actor['etatCivil']['infoNaissance']['paysNais'],
+			'death_date':       actor['etatCivil']['dateDeces'],
+			'job':              actor['profession']['libelleCourant'],
+			'job_category':     actor['profession']['socProcINSEE']['catSocPro'],
+			'job_family':       actor['profession']['socProcINSEE']['famSocPro'],
+			'hatvp_url':        actor['uri_hatvp'],
+		}
+
+		fields = [
+			'trigram',
+			'birth_city',
+			'birth_department',
+			'birth_country',
+			'death_date',
+			'job',
+			'job_family',
+		]
+
+		for field in fields:
+			if type(info[field]) != str:
+				del(info[field])
+
+		deputy, created = Deputy.objects.get_or_create(**info)
+
+		if type(actor['adresses']['adresse']) == list:
+			for address in actor['adresses']['adresse']:
+				self.parse_address(deputy, address)
+		else:
+			self.parse_address(deputy, actor['adresses']['adresse'])
+
+		if type(actor['mandats']['mandat']) == list:
+			for mandate in actor['mandats']['mandat']:
+				self.parse_mandate(deputy, mandate)
+		else:
+			self.parse_mandate(deputy, actor['mandats']['mandat'])
+
+	def parse_organs(self, folder):
+		parents = {}
+		for jsonfile in os.listdir(folder):
+			path = os.path.join(folder, jsonfile)
+			if os.path.isfile(path):
+				self.parse_organ(path, parents)
+		return parents
+
+	def parse_actors(self, folder):
+		for jsonfile in os.listdir(folder):
+			path = os.path.join(folder, jsonfile)
+			if os.path.isfile(path):
+				self.parse_actor(path)
+
+	def handle(self, *args, **kwargs):
+
+		self.fd = open('output.log', 'w')
+		self.url = 'https://data.assemblee-nationale.fr/static/openData/repository/15/amo/deputes_senateurs_ministres_legislature/AMO20_dep_sen_min_tous_mandats_et_organes_XV.json.zip'
+		self.folder = '/tmp'
+		self.filepath = os.path.join(self.folder, os.path.basename(self.url))
+		self.jsonfolder = os.path.join(self.folder, 'json')
+
+		self.download_data()
+		self.extract_data()
+
+		self.parse_parent_organs(self.parse_organs(os.path.join(self.jsonfolder, 'organe')))
+		self.parse_actors(os.path.join(self.jsonfolder, 'acteur'))
---
+++ src/django/deputies/models.py
@@ -0,0 +1,203 @@
+from django.db import models
+
+# DATA WE NEED
+# - countries
+# - french cities
+# - french departments
+# - french postal codes
+# - INSEE job categories
+# - INSEE job families
+
+CITY_MAX_LENGTH = 32
+UID_MAX_LENGTH = 10
+
+class Organ(models.Model):
+	TYPE_GROUPE_POLITIQUE                   = 'GroupePolitique_type'
+	TYPE_ORGANE_EXTERNE                     = 'OrganeExterne_Type'
+	TYPE_ORGANE_EXTRA_PARLEMENTAIRE         = 'OrganeExtraParlementaire_type'
+	TYPE_ORGANE_PARLEMENTAIRE               = 'OrganeParlementaire_Type'
+	TYPE_ORGANE_PARLEMENTAIRE_INTERNATIONAL = 'OrganeParlementaireInternational'
+
+	ORGAN_TYPE_CHOICES = (
+		(TYPE_GROUPE_POLITIQUE,                   'Groupe Politique'),
+		(TYPE_ORGANE_EXTERNE,                     'Organe Externe'),
+		(TYPE_ORGANE_EXTRA_PARLEMENTAIRE,         'Organe Extra Parlementaire'),
+		(TYPE_ORGANE_PARLEMENTAIRE,               'Orgaine Parlementaire'),
+		(TYPE_ORGANE_PARLEMENTAIRE_INTERNATIONAL, 'Organe Parlementaire International'),
+	)
+
+	CODE_TYPE_API          = 'API'
+	CODE_TYPE_ASSEMBLEE    = 'ASSEMBLEE'
+	CODE_TYPE_CJR          = 'CJR'
+	CODE_TYPE_CMP          = 'CMP'
+	CODE_TYPE_CNPE         = 'CNPE'
+	CODE_TYPE_CNPS         = 'CNPS'
+	CODE_TYPE_COMNL        = 'COMNL'
+	CODE_TYPE_COMPER       = 'COMPER'
+	CODE_TYPE_COMSENAT     = 'COMSENAT'
+	CODE_TYPE_COMSPSENAT   = 'COMSPSENAT'
+	CODE_TYPE_CONFPT       = 'CONFPT'
+	CODE_TYPE_CONSTITU     = 'CONSTITU'
+	CODE_TYPE_DELEG        = 'DELEG'
+	CODE_TYPE_DELEGBUREAU  = 'DELEGBUREAU'
+	CODE_TYPE_DELEGSENAT   = 'DELEGSENAT'
+	CODE_TYPE_GA           = 'GA'
+	CODE_TYPE_GE           = 'GE'
+	CODE_TYPE_GEVI         = 'GEVI'
+	CODE_TYPE_GOUVERNEMENT = 'GOUVERNEMENT'
+	CODE_TYPE_GP           = 'GP'
+	CODE_TYPE_GROUPESENAT  = 'GROUPESENAT'
+	CODE_TYPE_HCJ          = 'HCJ'
+	CODE_TYPE_MINISTERE    = 'MINISTERE'
+	CODE_TYPE_MISINFO      = 'MISINFO'
+	CODE_TYPE_MISINFOCOM   = 'MISINFOCOM'
+	CODE_TYPE_MISINFOPRE   = 'MISINFOPRE'
+	CODE_TYPE_OFFPAR       = 'OFFPAR'
+	CODE_TYPE_ORGAINT      = 'ORGAINT'
+	CODE_TYPE_ORGEXTPARL   = 'ORGEXTPARL'
+	CODE_TYPE_PARPOL       = 'PARPOL'
+	CODE_TYPE_PRESREP      = 'PRESREP'
+	CODE_TYPE_SENAT        = 'SENAT'
+
+	ORGAN_TYPE_CODE_CHOICES = (
+		(CODE_TYPE_API,          'Assemblée parlementaire internationale'),
+		(CODE_TYPE_ASSEMBLEE,    'Assemblée nationale'),
+		(CODE_TYPE_CJR,          'Cour de justice de la République'),
+		(CODE_TYPE_CMP,          'Commissions mixtes paritaires'),
+		(CODE_TYPE_CNPE,         'Commissions d’enquêtes'),
+		(CODE_TYPE_CNPS,         'Commissions spéciales'),
+		(CODE_TYPE_COMNL,        'Autres commissions permanentes'),
+		(CODE_TYPE_COMPER,       'Commissions permanentes législatives'),
+		(CODE_TYPE_COMSENAT,     'Commissions sénatoriales'),
+		(CODE_TYPE_COMSPSENAT,   'Commissions spéciales sénatoriales'),
+		(CODE_TYPE_CONFPT,       'Conférence des présidents'),
+		(CODE_TYPE_CONSTITU,     'Conseil constitutionnel'),
+		(CODE_TYPE_DELEG,        'Délégation parlementaire'),
+		(CODE_TYPE_DELEGBUREAU,  'Délégation du Bureau de l’Assemblée Nationale'),
+		(CODE_TYPE_DELEGSENAT,   'Délégation sénatoriale'),
+		(CODE_TYPE_GA,           'Groupe d’amitié'),
+		(CODE_TYPE_GE,           'Groupe d’études'),
+		(CODE_TYPE_GEVI,         'Groupe d’études à vocation internationale'),
+		(CODE_TYPE_GOUVERNEMENT, 'Gouvernement'),
+		(CODE_TYPE_GP,           'Groupe politique'),
+		(CODE_TYPE_GROUPESENAT,  'Groupe sénatorial'),
+		(CODE_TYPE_HCJ,          'Haute cour de justice'),
+		(CODE_TYPE_MINISTERE,    'Ministère'),
+		(CODE_TYPE_MISINFO,      'Missions d’informations'),
+		(CODE_TYPE_MISINFOCOM,   'Missions d’information communes'),
+		(CODE_TYPE_MISINFOPRE,   'Missions d’information de la conférence des Présidents'),
+		(CODE_TYPE_OFFPAR,       'Office parlementaire ou délégation mixte'),
+		(CODE_TYPE_ORGAINT,      'Organisme international'),
+		(CODE_TYPE_ORGEXTPARL,   'Organisme extra parlementaire'),
+		(CODE_TYPE_PARPOL,       'Parti politique'),
+		(CODE_TYPE_PRESREP,      'Présidence de la République'),
+		(CODE_TYPE_SENAT,        'Sénat'),
+	)
+
+	uid = models.CharField(max_length=UID_MAX_LENGTH)
+	type = models.CharField(max_length=40, choices=ORGAN_TYPE_CHOICES)
+	type_code = models.CharField(max_length=16, choices=ORGAN_TYPE_CODE_CHOICES)
+	label = models.CharField(max_length=2048)
+	label_edition = models.CharField(max_length=2048, null=True)
+	label_short = models.CharField(max_length=128)
+	label_abbrev = models.CharField(max_length=32)
+	date_start = models.DateField(null=True)
+	date_agreement = models.DateField(null=True)
+	date_end = models.DateField(null=True)
+	parent_organ = models.ForeignKey('deputies.Organ', on_delete=models.CASCADE, null=True)
+	chamber = models.CharField(max_length=32, null=True) # Always null
+	regime = models.CharField(max_length=200, null=True)
+	legislature = models.IntegerField(null=True)
+	secretary_1 = models.CharField(max_length=64, null=True)
+	secretary_2 = models.CharField(max_length=64, null=True)
+
+class Deputy(models.Model):
+	uid = models.CharField(max_length=UID_MAX_LENGTH)
+	civility = models.CharField(max_length=8)
+	first_name = models.CharField(max_length=32)
+	last_name = models.CharField(max_length=32)
+	alpha = models.CharField(max_length=32)
+	trigram = models.CharField(max_length=8)
+	birth_date = models.DateField()
+	birth_city = models.CharField(max_length=CITY_MAX_LENGTH)
+	birth_department = models.CharField(max_length=32)
+	birth_country = models.CharField(max_length=32)
+	death_date = models.DateField(null=True)
+	job = models.CharField(max_length=128)
+	job_category = models.CharField(max_length=200)
+	job_family = models.CharField(max_length=64)
+	hatvp_url = models.URLField(max_length=2048)
+
+class DeputyAddress(models.Model):
+	deputy = models.ForeignKey(Deputy, on_delete=models.CASCADE)
+	type = models.IntegerField()
+	type_label = models.CharField(max_length=64)
+	weight = models.IntegerField(null=True)
+	address_uid = models.CharField(max_length=8, null=True)
+	entitle = models.CharField(max_length=64, null=True)
+	street_number = models.CharField(max_length=16, null=True)
+	street_name = models.CharField(max_length=64, null=True)
+	street_addition = models.CharField(max_length=64, null=True)
+	postal_code = models.CharField(max_length=5, null=True)
+	city = models.CharField(max_length=CITY_MAX_LENGTH, null=True)
+	phone_number = models.CharField(max_length=32)
+	email_address = models.CharField(max_length=320)
+	web_site = models.CharField(max_length=2048)
+
+class Mandate(models.Model):
+
+	TYPE_MANDATE_MISSION        = 'MandatMission_Type'
+	TYPE_MANDATE_PARLEMENTAIRE  = 'MandatParlementaire_type'
+	TYPE_MANDATE_SIMPLE         = 'MandatSimple_Type'
+	TYPE_MANDATE_WITH_SUPPLEANT = 'MandatAvecSuppleant_Type'
+
+	MANDATE_CHOICES = (
+		(TYPE_MANDATE_MISSION,        'Mission'),
+		(TYPE_MANDATE_PARLEMENTAIRE,  'Parlementaire'),
+		(TYPE_MANDATE_SIMPLE,         'Simple'),
+		(TYPE_MANDATE_WITH_SUPPLEANT, 'Avec Suppléant'),
+	)
+
+	deputy = models.ForeignKey(Deputy, on_delete=models.CASCADE)
+	uid = models.CharField(max_length=UID_MAX_LENGTH)
+	mandate_type = models.CharField(max_length=24, choices=MANDATE_CHOICES)
+	legislature = models.IntegerField(null=True)
+	organ = models.ForeignKey(Organ, on_delete=models.CASCADE, null=True)
+	date_start = models.DateField(null=True)
+	date_end = models.DateField(null=True)
+	date_published = models.DateField(null=True)
+	precedence = models.IntegerField(null=True)
+	main_nomination = models.BooleanField()
+	quality_code = models.CharField(max_length=128, null=True)
+	quality_label = models.CharField(max_length=128)
+	quality_label_sex = models.CharField(max_length=128, null=True)
+	suppleant_uid = models.CharField(max_length=UID_MAX_LENGTH)
+	suppleant_date_start = models.DateField(null=True)
+	suppleant_date_end = models.DateField(null=True)
+	chamber = models.CharField(max_length=32, null=True) # Always null
+	election_region = models.CharField(max_length=32, null=True)
+	election_region_type = models.CharField(max_length=64, null=True)
+	election_department = models.CharField(max_length=32, null=True)
+	election_department_number = models.CharField(max_length=8, null=True)
+	election_circonscription = models.IntegerField(null=True)
+	mandate_cause = models.CharField(max_length=200, null=True)
+	circonscription_uid = models.CharField(max_length=UID_MAX_LENGTH, null=True)
+	mandate_date_start = models.DateField(null=True)
+	mandate_end_cause = models.CharField(max_length=200, null=True)
+	mandate_first_election = models.BooleanField(null=True)
+	mandate_hemicycle_seat = models.IntegerField(null=True)
+	mandate_replace_uid = models.CharField(max_length=UID_MAX_LENGTH, null=True)
+
+class MandateOrgan(models.Model):
+	mandate = models.ForeignKey(Mandate, on_delete=models.CASCADE)
+	organ = models.ForeignKey(Organ, on_delete=models.CASCADE, null=True)
+	organ_uid = models.CharField(max_length=UID_MAX_LENGTH)
+
+class MandateCollaborater(models.Model):
+	deputy = models.ForeignKey(Deputy, on_delete=models.CASCADE)
+	mandate = models.ForeignKey(Mandate, on_delete=models.CASCADE)
+	civility = models.CharField(max_length=8)
+	first_name = models.CharField(max_length=32)
+	last_name = models.CharField(max_length=32)
+	date_start = models.DateField(null=True)
+	date_end = models.DateField(null=True)
