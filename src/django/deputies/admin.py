49a5dcf7196751963a896010f3ac89bab63139bc
Corentin Delorme <codelorme@gmail.com>
2021-10-29 13:33:17 +0200

    Update Django app deputies: Implement admin models

--- src/django/deputies/admin.py
+++ src/django/deputies/admin.py
@@ -1,3 +1,28 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import *
+
+class OrganAdmin(admin.ModelAdmin):
+	pass
+
+class DeputyAdmin(admin.ModelAdmin):
+	pass
+
+class DeputyAddress(admin.ModelAdmin):
+	pass
+
+class MandateAdmin(admin.ModelAdmin):
+	pass
+
+class MandateOrganAdmin(admin.ModelAdmin):
+	pass
+
+class MandateCollaborator(admin.ModelAdmin):
+	pass
+
+admin.site.register(Organ, OrganAdmin)
+admin.site.register(Deputy, DeputyAdmin)
+admin.site.register(DeputyAddress, DeputyAddressAdmin)
+admin.site.register(Mandate, MandateAdmin)
+admin.site.register(MandateOrgan, MandateOrganAdmin)
+admin.site.register(MandateCollaborater, MandateCollaboraterAdmin)
