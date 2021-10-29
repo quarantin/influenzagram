9819128445bbba336c8a5c07ab2691dcb8717528
Corentin Delorme <codelorme@gmail.com>
2021-10-29 10:50:35 +0200

    Update MySQL init.sql: Explicitly set the database charset to utf8mb4 + Don't fail at user creation time when the user already exists

--- conf/mysql/init.sql
+++ conf/mysql/init.sql
@@ -1,3 +1,3 @@
-CREATE DATABASE influenzagram;
-CREATE USER 'influenzagram'@'%' IDENTIFIED BY 'influenzagram';
+CREATE DATABASE influenzagram CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci;
+CREATE USER IF NOT EXISTS 'influenzagram'@'%' IDENTIFIED BY 'influenzagram';
 GRANT ALL PRIVILEGES ON influenzagram.* TO 'influenzagram'@'%';
