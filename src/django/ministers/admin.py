082868766caba00738df1ef870a426c156a321fa
Corentin Delorme <codelorme@gmail.com>
2021-11-05 02:53:37 +0100

    Update django ministers admin: Fix list_display + Add search_fields

--- src/django/ministers/admin.py
+++ src/django/ministers/admin.py
@@ -3,6 +3,7 @@ from .models import Minister
 
 
 class MinisterAdmin(admin.ModelAdmin):
-	list_display = ('__str__',)
+	list_display = ('__str__', 'political_regime', 'head_of_state', 'nomination_start', 'nomination_end', 'function')
+	search_fields = ('first_name', 'last_name', 'function')
 
 admin.site.register(Minister, MinisterAdmin)
